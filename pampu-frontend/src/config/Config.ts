e085091922f1a4ce3a5b039060ec3da4394b6f85
Rohan <rhnshiva@gmail.com>
2022-07-18 22:08:07 -0500

    update micro url in frontend

--- pampu-frontend/src/config/Config.ts
+++ pampu-frontend/src/config/Config.ts
@@ -1,5 +1,5 @@
 const config = {
-    base: `https://pampu-backend_${window.location.hostname}`,
+    base: `https://pampu-backend_${window.location.hostname}/api`,
     add: "add",
     addFile: "add/file",
     fetch: "fetch",
