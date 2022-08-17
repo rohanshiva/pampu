7601d736951832f4e2a3ef6665ef5f3f5ea351e7
Rohan <rhnshiva@gmail.com>
2022-08-17 08:50:12 -0500

    use starlette cors

--- pampu-backend/main.py
+++ pampu-backend/main.py
@@ -21,7 +21,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "OPTIONS", "DELETE"],
     allow_headers=["Content-Type"],
 )
 
